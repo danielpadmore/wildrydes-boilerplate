73fc455a388856e8dce49847c61bce83e0cd15fa
Daniel Padmore <daniel@popsa.com>
2019-09-06 17:08:13 +0100

    init

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "eu-west-2_6yLaHi3uV", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "1d6o796b64prcqugnf6418ugu2", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "eu-west-2" // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: " https://b2ljuqr2g5.execute-api.eu-west-2.amazonaws.com/prod" // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  }
+};
